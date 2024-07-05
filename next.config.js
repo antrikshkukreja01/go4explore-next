3ac65a4468e21bdeb563f4215e0efc93d6d3d3b2
antrikshkukreja01 <antrikshkukreja01@gmail.com>
2024-07-05 14:16:45 +0530

    ln-chnage

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
-// next.config.js
-
 const path = require("path");
 
 module.exports = {
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.(mp4|webm|ogg|swf|ogv)$/,
